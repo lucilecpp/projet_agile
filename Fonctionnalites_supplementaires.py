d442124166300e256ec6a3a39b349e62d065920c
Monica35580 <91956878+Monica35580@users.noreply.github.com>
2023-12-25 00:12:52 +0100

    Fonctionnalités qui ne sont pas dans le code principal

--- Fonctionnalites_supplementaires.py
+++ Fonctionnalites_supplementaires.py
@@ -3,7 +3,8 @@ from PIL import Image, ImageTk
 import cairosvg
 import os
 
-'''class MaClasse:
+'''#classe pour remplacer les boutons de vote par une image de la carte
+class BoutonImage:
     def __init__(self, master):
         self.master = master
         self.liste_valeur = [0, 1, 2, 3, 5, 8, 13, 20, 40, 100, "interro", "cafe"]
@@ -45,99 +46,93 @@ import os
         print(f"Valeur sélectionnée : {value}")
 if __name__ == "__main__":
     root = Tk()
-    app = MaClasse(root)
+    app = BoutonImage(root)
     root.mainloop()'''
 
-'''class BlocNotes:
-    def __init__(self, master):
-        self.master = master
-        master.title("Block note")
+import tkinter as tk
+from tkinter import ttk, Tk
 
-        self.texte = ""
-
-        # Zone de saisie
-        self.entree_texte = Entry(master, width=30)
-        self.entree_texte.pack(pady=20)
-
-        # Bouton pour valider le texte
-        self.bouton_valider = Button(master, text="Valider", command=self.afficher_texte)
-        self.bouton_valider.pack()
-
-        # Label pour afficher le texte validé
-        self.label_texte = Label(master, text="")
-        self.label_texte.pack(pady=20)
-
-        # Agrandir la fenêtre
-        master.geometry("600x500")
-
-    def afficher_texte(self):
-        # Récupérer le texte de la zone de saisie
-        nouveau_texte = self.entree_texte.get()
-
-        # Ajouter le nouveau texte au texte existant
-        self.texte += "\n" + nouveau_texte
-
-        # Afficher le texte dans le label
-        self.label_texte.config(text=self.texte)
-
-        # Effacer le texte de la zone de saisie
-        self.entree_texte.delete(0, "end")
-if __name__ == "__main__":
-    root = tk()
-    appBlocNotes = BlocNotes(root)
-    root.mainloop()'''
-
-
-'''class MinuterieApp:
-    def __init__(self, fenetre, duree):
+'''class CompteAReboursApp:
+    def __init__(self, fenetre):
         self.fenetre = fenetre
-        self.fenetre.title("Minuterie")
+        self.fenetre.title("Compte à Rebours")
 
         self.temps_restant = tk.StringVar()
-        self.temps_restant.set(self.format_duree(duree))
 
         self.label_temps = tk.Label(fenetre, textvariable=self.temps_restant, font=("Helvetica", 48))
         self.label_temps.pack(pady=20)
 
-        self.bouton_demarrer = tk.Button(fenetre, text="Démarrer", command=self.demarrer_minuterie)
-        self.bouton_demarrer.pack(pady=10)
+        self.bouton_demarrer_arreter = tk.Button(fenetre, text="Démarrer", command=self.demarrer_arreter_compte_a_rebours)
+        self.bouton_demarrer_arreter.pack(pady=10)
 
-        self.bouton_arreter = tk.Button(fenetre, text="Arrêter", command=self.arreter_minuterie, state=tk.DISABLED)
-        self.bouton_arreter.pack(pady=10)
+        self.duree = 0
+        self.temps_restant_sec = 0
+        self.compte_a_rebours_actif = False
 
-        self.duree = duree
-        self.temps_restant_sec = duree
-        self.minuterie_active = False
+        self.selecteur_minutes = None
+        self.selecteur_secondes = None
+
+        self.creer_selecteur_temps()
 
     def format_duree(self, secondes):
         minutes, secondes = divmod(secondes, 60)
         return f"{minutes:02}:{secondes:02}"
 
     def mise_a_jour_temps(self):
-        self.temps_restant_sec -= 1
-        self.temps_restant.set(self.format_duree(self.temps_restant_sec))
+        if self.compte_a_rebours_actif:
+            self.temps_restant_sec -= 1
+            self.temps_restant.set(self.format_duree(self.temps_restant_sec))
 
-        if self.temps_restant_sec <= 0:
-            self.arreter_minuterie()
+            if self.temps_restant_sec <= 0:
+                self.arreter_compte_a_rebours()
+            else:
+                self.fenetre.after(1000, self.mise_a_jour_temps)
 
-        if self.minuterie_active:
-            self.fenetre.after(1000, self.mise_a_jour_temps)
+    def demarrer_arreter_compte_a_rebours(self):
+        if self.compte_a_rebours_actif:
+            self.arreter_compte_a_rebours()
+        else:
+            self.demarrer_compte_a_rebours()
 
-    def demarrer_minuterie(self):
-        self.minuterie_active = True
-        self.bouton_demarrer.config(state=tk.DISABLED)
-        self.bouton_arreter.config(state=tk.NORMAL)
-        self.mise_a_jour_temps()
-
-    def arreter_minuterie(self):
-        self.minuterie_active = False
-        self.bouton_demarrer.config(state=tk.NORMAL)
-        self.bouton_arreter.config(state=tk.DISABLED)
+    def demarrer_compte_a_rebours(self):
+        self.duree = int(self.selecteur_minutes.get()) * 60 + int(self.selecteur_secondes.get())
         self.temps_restant_sec = self.duree
         self.temps_restant.set(self.format_duree(self.duree))
+        self.compte_a_rebours_actif = True
+        self.bouton_demarrer_arreter.config(text="Arrêter", bg="red")
+        self.mise_a_jour_temps()
+
+    def arreter_compte_a_rebours(self):
+        self.compte_a_rebours_actif = False
+        self.bouton_demarrer_arreter.config(text="Démarrer", bg="green")
+        self.temps_restant_sec = self.duree
+        self.temps_restant.set(self.format_duree(self.duree))
+
+    def creer_selecteur_temps(self):
+        frame_selecteur = ttk.Frame(self.fenetre)
+        frame_selecteur.pack(pady=20)
+
+        label_selecteur = ttk.Label(frame_selecteur, text="Durée du compte à rebours :")
+        label_selecteur.grid(row=0, column=0, padx=10)
+
+        self.selecteur_minutes = ttk.Spinbox(frame_selecteur, from_=0, to=59, wrap=True, width=2)
+        self.selecteur_minutes.grid(row=0, column=1, padx=10)
+
+        label_minutes = ttk.Label(frame_selecteur, text="minutes")
+        label_minutes.grid(row=0, column=2, padx=10)
+
+        self.selecteur_secondes = ttk.Spinbox(frame_selecteur, from_=0, to=59, wrap=True, width=2)
+        self.selecteur_secondes.grid(row=0, column=3, padx=10)
+
+        label_secondes = ttk.Label(frame_selecteur, text="secondes")
+        label_secondes.grid(row=0, column=4, padx=10)
+
+        self.selecteur_minutes.set(0)
+        self.selecteur_secondes.set(0)
+
 if __name__ == "__main__":
-    duree_minuterie = 300  # 5 minutes
-    fenetre_principale = tk.tk()
-    appMinuterie = MinuterieApp(fenetre_principale, duree_minuterie)
+    fenetre_principale = Tk()
+    appCompteARebours = CompteAReboursApp(fenetre_principale)
     fenetre_principale.mainloop()'''
+
 
